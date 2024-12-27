6b1ccb696cd79daf6fac423e664e64f2a5234c97
kishan.maharana <kishan@codebuddy.co>
2024-12-27 16:17:49 +0530

    test cases added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { BookModule } from './book/book.module';
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [AuthorModule, BookModule],
+  imports: [AuthorModule, BookModule, TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
